986bf11465ad568ec8ca89b121f615d4e8667d63
romafilyutitch <roma050399@yadnex.ru>
2021-05-25 19:08:06 +0300

    main started

--- src/main/java/com/epam/jwd/final_task/app/Main.java
+++ src/main/java/com/epam/jwd/final_task/app/Main.java
@@ -5,6 +5,10 @@ import com.epam.jwd.final_task.exception.ConnectionPoolInitializationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
@@ -12,6 +16,15 @@ public class Main {
         logger.trace("App is started");
         ConnectionPool pool = ConnectionPool.getConnectionPool();
         pool.init();
+        List<Connection> connections = new ArrayList<>();
+        for (int i = 0; i < 27; i++) {
+            connections.add(pool.takeFreeConnection());
+        }
+        Thread.sleep(10000);
+        for (int i = 0; i < 25; i++) {
+            pool.returnTakenConnection(connections.get(i));
+        }
+        Thread.sleep(50000);
         pool.destroy();
         logger.trace("App is end");
     }
